9e9bcc2eaf098a5e19f46e98b7541ab405f4b0c7
Harikrishnan-TM <techdatamaxonline@gmail.com>
2025-07-08 07:35:53 -0700

    fixing database

--- stocks/models.py
+++ stocks/models.py
@@ -15,7 +15,8 @@ class VirtualStock(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE)  # link to real Stock
     quantity = models.PositiveIntegerField(default=0)
-    buy_price = models.DecimalField(max_digits=10, decimal_places=2)
+    #buy_price = models.DecimalField(max_digits=10, decimal_places=2)
+    buy_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return f"{self.user.username} - {self.stock.symbol}"
@@ -38,7 +39,8 @@ class UserTransaction(models.Model):
     )
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    stock = models.ForeignKey(VirtualStock, on_delete=models.CASCADE)
+    #stock = models.ForeignKey(VirtualStock, on_delete=models.CASCADE)
+    stock = models.ForeignKey(VirtualStock, on_delete=models.CASCADE, null=True, blank=True)
     transaction_type = models.CharField(max_length=4, choices=TRANSACTION_TYPES)
     quantity = models.PositiveIntegerField()
     price_at_execution = models.FloatField()

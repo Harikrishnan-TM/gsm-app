d04db71f1a471fab1ed6634dfa7342ea89f69b6c
Harikrishnan-TM <techdatamaxonline@gmail.com>
2025-07-08 05:51:32 -0700

    updating settings.py fixing csrf problem

--- core/settings.py
+++ core/settings.py
@@ -149,3 +149,8 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://gsm-backend.fly.dev",
+]

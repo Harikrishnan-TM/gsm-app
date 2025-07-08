178cafea5cb95bed571173978c08478a8560ea08
Harikrishnan-TM <techdatamaxonline@gmail.com>
2025-07-08 06:21:04 -0700

    updated stocks urls

--- stocks/urls.py
+++ stocks/urls.py
@@ -3,9 +3,9 @@ from rest_framework.routers import DefaultRouter
 from .views import VirtualStockViewSet, UserPortfolioViewSet, UserTransactionViewSet
 
 router = DefaultRouter()
-router.register(r'stocks', VirtualStockViewSet)
-router.register(r'portfolio', UserPortfolioViewSet)
-router.register(r'transactions', UserTransactionViewSet)
+router.register(r'portfolio', UserPortfolioViewSet, basename='portfolio')
+router.register(r'transactions', UserTransactionViewSet, basename='transaction')
+router.register(r'stocks', VirtualStockViewSet, basename='stock')
 
 urlpatterns = [
     path('', include(router.urls)),

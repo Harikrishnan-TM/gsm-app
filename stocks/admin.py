adfe6531e67962d8fbe4cad30c6d729aa3feeadd
Harikrishnan-TM <techdatamaxonline@gmail.com>
2025-07-09 04:02:01 -0700

    import problem fixed

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import VirtualStock, UserPortfolio, UserTransaction
+from .models import PriceHistory
 
 admin.site.register(VirtualStock)
 admin.site.register(UserPortfolio)

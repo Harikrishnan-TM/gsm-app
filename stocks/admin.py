8327a9061f89887e38487134635ef547823498f8
Harikrishnan-TM <techdatamaxonline@gmail.com>
2025-07-09 03:43:58 -0700

    admin registering

--- stocks/admin.py
+++ stocks/admin.py
@@ -4,3 +4,4 @@ from .models import VirtualStock, UserPortfolio, UserTransaction
 admin.site.register(VirtualStock)
 admin.site.register(UserPortfolio)
 admin.site.register(UserTransaction)
+admin.site.register(PriceHistory)  

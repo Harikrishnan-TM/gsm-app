239dc54ddc58dc7e5d4f6e303773137d0aa60423
Harikrishnan-TM <techdatamaxonline@gmail.com>
2025-07-10 03:38:51 -0700

    updating admin.py

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import VirtualStock, UserPortfolio, UserTransaction
 from .models import PriceHistory
+from .models import Stock
 
+admin.site.register(Stock)
 admin.site.register(VirtualStock)
 admin.site.register(UserPortfolio)
 admin.site.register(UserTransaction)
